174ce012231df29e99d25df87a435654617812bf
Marcus Hanses <marcus.hanses@intergiro.com>
2021-06-21 16:27:19 +0200

    Renamed Customer to Contact.

--- Contact/index.spec.ts
+++ Contact/index.spec.ts
@@ -1,6 +1,6 @@
 import * as model from "../index"
 
-describe("Customer", () => {
+describe("Contact", () => {
 	const tom = {
 		type: "person",
 		identitityNumber: "198506086118",
@@ -13,11 +13,11 @@ describe("Customer", () => {
 			countryCode: "SE",
 		},
 	}
-	it("is tom", () => expect(model.Customer.is(tom)).toBe(true))
-	it("flaw tom", () => expect(model.Customer.flaw(tom)).toEqual({ flaws: [], type: "model.Customer" }))
-	it("is customer", () =>
+	it("is tom", () => expect(model.Contact.is(tom)).toBe(true))
+	it("flaw tom", () => expect(model.Contact.flaw(tom)).toEqual({ flaws: [], type: "model.Contact" }))
+	it("is contact", () =>
 		expect(
-			model.Customer.is({
+			model.Contact.is({
 				type: "person",
 				identitityNumber: "198506086118",
 				id: "KUjNHzpvmp9oAmW+SzX8Rhr32BNpYLRdWv_to6gBX8M=",
@@ -28,9 +28,9 @@ describe("Customer", () => {
 				countryCode: "MT",
 			})
 		).toBeTruthy())
-	it("is customer", () =>
+	it("is contact", () =>
 		expect(
-			model.Customer.is({
+			model.Contact.is({
 				type: "person",
 				identitityNumber: "198506086118",
 				id: "KUjNHzpvmp9oAmW+SzX8Rhr32BNpYLRdWv_to6gBX8M=",
@@ -39,9 +39,9 @@ describe("Customer", () => {
 				countryCode: "MT",
 			})
 		).toBeTruthy())
-	it("is customer", () =>
+	it("is contact", () =>
 		expect(
-			model.Customer.is({
+			model.Contact.is({
 				type: "person",
 				identitityNumber: "198506086118",
 				id: "KUjNHzpvmp9oAmW+SzX8Rhr32BNpYLRdWv_to6gBX8M=",
