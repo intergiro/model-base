5bdbcdb6cc5cbf0e2ee46f35eb0c14368deb3f3b
Magnus Gustafsson <magnus.ja.gustafsson@gmail.com>
2021-08-19 13:17:06 +0200

    Create connection (#25)
    Co-authored-by: Magnus Gustafsson <magnus.gustafsson@intergiro.com>

---
+++ Connection.ts
@@ -0,0 +1,46 @@
+import * as gracely from "gracely"
+import { default as fetch } from "isomorphic-fetch"
+
+export class Connection {
+	private constructor(readonly url: string, readonly token: string) {}
+
+	async fetch<Response>(
+		path: string,
+		method: string,
+		request?: any,
+		tokenize?: true
+	): Promise<Response | gracely.Error> {
+		const response = await fetch(`${this.url}/${path}`, {
+			method,
+			headers: {
+				Accept: tokenize ? "application/jwt+camelCase" : "application/json+camelCase",
+				"Content-Type": "application/json; charset=utf-8",
+				Authorization: `Bearer ${this.token}`,
+			},
+			body: JSON.stringify(request),
+		}).catch(_ => undefined)
+		return !response
+			? gracely.server.unavailable()
+			: response.headers.get("Content-Type")?.startsWith("application/json")
+			? response.json()
+			: response.text()
+	}
+	async get<Response>(path: string): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "GET")
+	}
+	async patch<Response>(path: string, request: any): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "PATCH", request)
+	}
+	async post<Response>(path: string, request: any, tokenize?: true): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "POST", request, tokenize)
+	}
+	async put<Response>(path: string, request: any): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "PUT", request)
+	}
+	async remove<Response>(path: string): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "DELETE")
+	}
+	static open(url: string | undefined, token: string | undefined): Connection | undefined {
+		return token && url ? new Connection(url, token) : undefined
+	}
+}
