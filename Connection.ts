a81427a39752fef7b8d9cf1b6aa17fb3d7fae1bb
Magnus Gustafsson <magnus.gustafsson@intergiro.com>
2021-08-20 11:50:09 +0200

    Add optional header for Connection

--- Connection.ts
+++ Connection.ts
@@ -1,6 +1,9 @@
 import * as gracely from "gracely"
 import { default as fetch } from "isomorphic-fetch"
 
+interface Header {
+	accept?: "application/pdf" | "text/html"
+}
 export class Connection {
 	private constructor(readonly url: string, readonly token: string) {}
 
@@ -8,12 +11,13 @@ export class Connection {
 		path: string,
 		method: string,
 		request?: any,
-		tokenize?: true
+		tokenize?: true,
+		header?: Header
 	): Promise<Response | gracely.Error> {
 		const response = await fetch(`${this.url}/${path}`, {
 			method,
 			headers: {
-				Accept: tokenize ? "application/jwt+camelCase" : "application/json+camelCase",
+				Accept: header?.accept ?? (tokenize ? "application/jwt" : "application/json") + "+camelCase",
 				"Content-Type": "application/json; charset=utf-8",
 				Authorization: `Bearer ${this.token}`,
 			},
@@ -25,17 +29,22 @@ export class Connection {
 			? response.json()
 			: response.text()
 	}
-	async get<Response>(path: string): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "GET")
+	async get<Response>(path: string, header?: Header): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "GET", undefined, undefined, header)
 	}
-	async patch<Response>(path: string, request: any): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "PATCH", request)
+	async patch<Response>(path: string, request: any, header?: Header): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "PATCH", request, undefined, header)
 	}
-	async post<Response>(path: string, request: any, tokenize?: true): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "POST", request, tokenize)
+	async post<Response>(
+		path: string,
+		request: any,
+		tokenize?: true,
+		header?: Header
+	): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "POST", request, tokenize, header)
 	}
-	async put<Response>(path: string, request: any): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "PUT", request)
+	async put<Response>(path: string, request: any, header?: Header): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "PUT", request, undefined, header)
 	}
 	async remove<Response>(path: string): Promise<Response | gracely.Error> {
 		return await this.fetch<Response>(path, "DELETE")
