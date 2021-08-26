30edd603a10edf6e90d08fddeb0e209d3efeb8c3
Magnus Gustafsson <magnus.ja.gustafsson@gmail.com>
2021-08-26 15:43:00 +0200

    Connection change fetch parameter to accept (#33)
    * Add optional header to Connection
    
    * fetch use accept param instead of tokenize, header
    
    Co-authored-by: Magnus Gustafsson <magnus.gustafsson@intergiro.com>

--- Connection.ts
+++ Connection.ts
@@ -1,5 +1,7 @@
 import * as gracely from "gracely"
 
+type Accept = "jwt" | "json" | "pdf" | "html"
+
 export class Connection {
 	private constructor(readonly url: string, readonly token: string) {}
 
@@ -7,12 +9,14 @@ export class Connection {
 		path: string,
 		method: string,
 		request?: any,
-		tokenize?: true
+		accept: Accept = "json"
 	): Promise<Response | gracely.Error> {
 		const response = await fetch(`${this.url}/${path}`, {
 			method,
 			headers: {
-				Accept: tokenize ? "application/jwt+camelCase" : "application/json+camelCase",
+				Accept:
+					{ jwt: "application/jwt", json: "application/json", pdf: "application/pdf", html: "text/html" }[accept] +
+					"+camelCase",
 				"Content-Type": "application/json; charset=utf-8",
 				Authorization: `Bearer ${this.token}`,
 			},
@@ -24,20 +28,20 @@ export class Connection {
 			? response.json()
 			: response.text()
 	}
-	async get<Response>(path: string): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "GET")
+	async get<Response>(path: string, accept?: Accept): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "GET", undefined, accept)
 	}
-	async patch<Response>(path: string, request: any): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "PATCH", request)
+	async patch<Response>(path: string, request: any, accept?: Accept): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "PATCH", request, accept)
 	}
-	async post<Response>(path: string, request: any, tokenize?: true): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "POST", request, tokenize)
+	async post<Response>(path: string, request: any, accept?: Accept): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "POST", request, accept)
 	}
-	async put<Response>(path: string, request: any): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "PUT", request)
+	async put<Response>(path: string, request: any, accept?: Accept): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "PUT", request, accept)
 	}
-	async remove<Response>(path: string): Promise<Response | gracely.Error> {
-		return await this.fetch<Response>(path, "DELETE")
+	async remove<Response>(path: string, accept?: Accept): Promise<Response | gracely.Error> {
+		return await this.fetch<Response>(path, "DELETE", undefined, accept)
 	}
 	static open(url: string | undefined, token: string | undefined): Connection | undefined {
 		return token && url ? new Connection(url, token) : undefined
