b774c3b6dd0bafc785a73c615800f8dbcb7e4469
Robin Nedergård <robin.nedergard@payfunc.com>
2021-11-26 15:26:50 +0100

    Fixed some errors returning a string

--- Connection.spec.ts
+++ Connection.spec.ts
@@ -1,9 +1,40 @@
 import * as model from "./Connection"
 
+jest.setTimeout(50000)
 describe("connection test", () => {
-	it("open", () => {
-		const connection = model.Connection.open("", "")
+	it("calling non existing endpoint", async () => {
+		const connection = model.Connection.open("https://merchant.intergiro.com/v1", "")
 		expect(connection).toBeTruthy()
-		expect(connection.get(model.Connection?.url ?? "")).toBeTruthy()
+		interface ReturnType {
+			something: string
+		}
+		expect(await connection.get<ReturnType>("/notexistingendpoint")).toEqual({
+			status: 404,
+			type: "not found",
+		})
+	})
+	it.skip("open incorrect url (might get string)", async () => {
+		const connection = model.Connection.open("https://merchant.intergiro.com/v", "")
+		expect(connection).toBeTruthy()
+		interface ReturnType {
+			something: string
+		}
+		expect(await connection.get<ReturnType>("/notexistingendpoint")).toEqual({
+			status: 500,
+			type: "unknown error",
+			details: expect.any(String),
+		})
+	})
+	it("open unauthorized", async () => {
+		const connection = model.Connection.open("https://merchant.intergiro.com/v1", "")
+		expect(connection).toBeTruthy()
+		interface ReturnType {
+			something: string
+		}
+		expect(await connection.get<ReturnType>("/authorization")).toEqual({
+			error: "unauthorized",
+			status: 401,
+			type: "not authorized",
+		})
 	})
 })
